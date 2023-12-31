93664f23d2ba0a96f1a21b398973b76a4a6efd06
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-12-31 09:22:09 +0530

    html

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      extend: {
+        backgroundImage: {
+          'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+          'gradient-conic':
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            'icon': "url('')",
+  
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
